6b7290ac3778987e2b84eb6e9dc3c71990df4a1d
Camya <camyacodes@gmail.com>
2022-04-08 15:15:53 -0400

    get users and create user controller functions

---
+++ controllers/user-controller.js
@@ -0,0 +1,40 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+  
+  getAllUser(req, res) {
+    User.find({})
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // get one User by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No User found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // create User
+  createUser({ body }, res) {
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  },
+
+
+};
+
+module.exports = userController
