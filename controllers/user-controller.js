fbb59073b468e5cfc91b1cf854f361635f7a1cdc
Camya <camyacodes@gmail.com>
2022-04-09 12:32:05 -0400

    add and remove friend routes added and tested

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,18 +7,25 @@ const userController = {
 			.catch((err) => res.status(400).json(err));
 	},
 
-	// get one User by id
+	// get one user by id
 	getUserById({ params }, res) {
 		User.findOne({ _id: params.id })
-			.then((dbUserData) => {
-				if (!dbUserData) {
-					res.status(404).json({ message: "No User found with this id!" });
-					return;
-				}
-				res.json(dbUserData);
-			})
-			.catch((err) => res.status(400).json(err));
-	},
+		  .populate({
+			path: 'friends',
+			select: '-__v'
+		  })
+		  .then(dbUserData => {
+			if (!dbUserData) {
+			  res.status(404).json({ message: 'No User found with this id!' });
+			  return;
+			}
+			res.json(dbUserData);
+		  })
+		  .catch(err => {
+			console.log(err);
+			res.sendStatus(400);
+		  });
+	  },
 
 	// create User
 	createUser({ body }, res) {
@@ -51,7 +58,33 @@ const userController = {
 			res.json(dbUserData);
 		  })
 		  .catch(err => res.status(400).json(err));
-	  },
+	},
+	 //add friend
+     addFriend({ params }, res) {
+		User.findOneAndUpdate(
+			{_id: params.userId},
+			{ $push: { friends: params.friendId } },
+			{ new: true }
+		)
+		.then(dbUserData => {
+			if (!dbUserData) {
+				res.status(404).json({ message: 'No user found with this ID!' });
+				return;
+			}
+			res.json(dbUserData);
+		})
+		.catch(err => res.json(err));
+	},
+	     //remove Friend
+		 removeFriend( { params }, res) {
+			User.findOneAndUpdate(
+				{ _id: params.userId },
+				{ $pull: { friends: params.friendId }},
+				{ new: true}
+			)
+			.then(dbUserData => res.json(dbUserData))
+			.catch(err => res.json(err));
+		},
 };
 
 module.exports = userController;
